86e413b3c0554f0838c0b4691004a223656e7611
sivaprakash007 <65717036+sivaprakash007@users.noreply.github.com>
2020-05-26 14:01:23 +0530

    Add files via upload

---
+++ siva calculator.py
@@ -0,0 +1,28 @@
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+
+def add(x,y):
+    c=x+y
+    return c
+result=add(x,y)
+print('add values',result)
+def subtract(x,y):
+    c=x+y
+    return c
+result=add(x,y)
+print('subtract values',result)
+def multiply(x,y):
+    c=x*y
+    return c
+result=multiply(x,y)
+print('multiply values',result)
+def remainder(x,y):
+    c=x%y
+    return c
+result=remainder(x,y)
+print('remainder values',result)
+def quotient(x,y):
+    c=x//y
+    return c
+result=quotient(x,y)
+print('quotient values',result)
